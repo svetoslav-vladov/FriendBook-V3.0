06508377707f023fad114984875d43a33b5aa16a
Svetoslav Vladov <svetoslavladov@gmail.com>
2018-04-26 02:06:41 +0300

    Minor js fixes nav and main

--- app/view/main.php
+++ app/view/main.php
@@ -12,7 +12,7 @@
             </div>
             <!--/span-->
             <div class="col-md-6" id="#main">
-                <div class="post_modal">
+                <div class="post_modal mb-5">
                     <?php require_once '../app/include/modules/post-mod.php';?>
                 </div>
                 <hr>
